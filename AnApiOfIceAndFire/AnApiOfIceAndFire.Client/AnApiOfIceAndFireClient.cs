8e407292716995e926f7e50fd0cb45357adb15b0
Nuno Costa <nunofilipecosta@users.noreply.github.com>
2021-09-18 11:24:03 +0100

    BULK COMMIT

---
+++ AnApiOfIceAndFire/AnApiOfIceAndFire.Client/AnApiOfIceAndFireClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnApiOfIceAndFire.Client
+{
+    public class AnApiOfIceAndFireClient
+    {
+        private readonly HttpClient client;
+
+        public AnApiOfIceAndFireClient() 
+        {
+            this.client = HttpClientFactory.Create();
+            this.client.BaseAddress = new Uri("https://anapioficeandfire.com/api/");
+            this.client.Timeout = new TimeSpan(0, 0, 30);
+            this.client.DefaultRequestHeaders.Clear();
+        }
+    }
+}
